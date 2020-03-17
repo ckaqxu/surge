6bf25b7be005f4b5bc3fb3d044cdd8b1f005bb4f
ckaqxu <ckaqxu8023@gmail.com>
2020-03-17 21:56:07 +0800

    Update eval_script.js

--- eval_script.js
+++ eval_script.js
@@ -39,6 +39,8 @@ https://raw.githubusercontent.com/ckaqxu/surge/ckaqxu-sub/sub_script4.conf
 [eval_local]
 // custom local...
 
+http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Kuwo.js
+
 // response ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig) eval https://raw.githubusercontent.com/yichahucha/surge/master/jd_price.js
 
 
